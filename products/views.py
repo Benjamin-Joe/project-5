a1852241362c947d77317b0cc0db83d9c0537bd3
Benjamin-Joe <benjaminjoe43@gmail.com>
2022-02-24 22:37:29 +0000

    Create product detail functionality for each item

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 "Views.py In Products App"
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 
@@ -10,3 +10,12 @@ def all_products(request):
         'products': products,
     }
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    "A view to see the details on a specific product"
+    product = get_object_or_404(Product, pk=product_id)
+    context = {
+        'product': product,
+    }
+    return render(request, 'products/product_detail.html', context)
