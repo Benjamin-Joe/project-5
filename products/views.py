8db20bfb2ebd63029e25c5a934750b38f70d236d
Benjamin-Joe <benjaminjoe43@gmail.com>
2022-02-25 00:50:11 +0000

    Add functionality for search in nav bar form

--- products/views.py
+++ products/views.py
@@ -1,13 +1,27 @@
 "Views.py In Products App"
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404, reverse
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product
 
 
 def all_products(request):
     "A view to see the store's products, including search abilities"
     products = Product.objects.all()
+    query = None
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "Nothing Entered!")
+                return redirect(reverse('products'))
+
+            queries = Q(title__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
+    
     context = {
         'products': products,
+        'search_term': query,
     }
     return render(request, 'products/products.html', context)
 
@@ -15,6 +29,7 @@ def all_products(request):
 def product_detail(request, product_id):
     "A view to see the details on a specific product"
     product = get_object_or_404(Product, pk=product_id)
+
     context = {
         'product': product,
     }
