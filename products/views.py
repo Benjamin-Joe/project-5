451dace870e20b6f47f0c93cc3454d463fd3f17f
Benjamin-Joe <benjaminjoe43@gmail.com>
2022-02-25 16:25:39 +0000

    Add sort by price and rating functionality, remove deals dropdown from second-nav.html

--- products/views.py
+++ products/views.py
@@ -6,11 +6,30 @@ from .models import Product, Category
 
 
 def all_products(request):
-    "A view to see the store's products, including search abilities"
+    """
+    A view to see the store's products, including search abilities
+   Large portion of all_products code came from Code Institute's Boutique Aho
+    """
     products = Product.objects.all()
+    sort = None
+    direction = None
     query = None
     categories = None
     if request.GET:
+
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
@@ -23,11 +42,12 @@ def all_products(request):
 
             queries = Q(title__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
-    
+    current_sorting = f'{sort}_{direction}'
     context = {
         'products': products,
         'search_term': query,
         'current_categories': categories,
+        'current_sorting': current_sorting,
 
     }
     return render(request, 'products/products.html', context)
