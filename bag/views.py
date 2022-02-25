87e3687441b7340f73574fc4a05ea95db4fad929
Benjamin-Joe <benjaminjoe43@gmail.com>
2022-02-25 23:48:15 +0000

    Update subtotal price with product quantity

--- bag/views.py
+++ bag/views.py
@@ -1,5 +1,5 @@
 "Views.py In Bag App"
-from django.shortcuts import render, redirect
+from django.shortcuts import HttpResponse, render, reverse, redirect
 
 
 def add_to_bag(request, item_id):
@@ -15,7 +15,31 @@ def add_to_bag(request, item_id):
     return redirect(redirect_url)
 
 
+def adjust_bag(request, item_id):
+    "Adjusting items in shopping bag"
+    quantity = int(request.POST.get('quantity'))
+    bag = request.session.get('bag', {})
+
+    if quantity > 0:
+        bag[item_id] = quantity
+    else:
+        del bag[item_id]
+        bag.pop(item_id)
+    return redirect(reverse('view_bag'))
+
+
 def view_bag(request):
     "Shopping bag view"
     return render(request, 'bag/bag.html')
 
+
+def remove_from_bag(request, item_id):
+    "Removing items from shopping bag"
+    try:
+        bag = request.session.get('bag', {})
+        bag.pop(item_id)
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+    except Exception as e:
+        return HttpResponse(status=500)
