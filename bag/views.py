cf24dd6cb1e4d932e98bbf96bc48ce231522690b
Benjamin-Joe <benjaminjoe43@gmail.com>
2022-02-26 22:47:08 +0000

    Add messages for adding and removing items from basket. Code for toast_success directly taken from code institute

--- bag/views.py
+++ bag/views.py
@@ -1,5 +1,5 @@
 "Views.py In Bag App"
-from django.shortcuts import HttpResponse, render, reverse, redirect
+from django.shortcuts import HttpResponse, get_object_or_404, render, reverse, redirect
 from django.contrib import messages
 from products.models import Product
 
@@ -7,15 +7,16 @@ from products.models import Product
 def add_to_bag(request, item_id):
     "Adding multiple items to shopping bag"
 
-    product = Product.objects.get(pk=item_id)
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
     if item_id in list(bag.keys()):
-        bag[item_id] += quantity
         messages.success(request, f'Added {product.title} to your bag')
+        bag[item_id] += quantity
 
     else:
+        messages.success(request, f'Added {product.title} to your bag')
         bag[item_id] = quantity
     request.session['bag'] = bag
     return redirect(redirect_url)
@@ -41,11 +42,15 @@ def view_bag(request):
 
 def remove_from_bag(request, item_id):
     "Removing items from shopping bag"
+    product = get_object_or_404(Product, pk=item_id)
+
     try:
         bag = request.session.get('bag', {})
         bag.pop(item_id)
+        messages.success(request, f'Removed {product.title} to your bag')
 
         request.session['bag'] = bag
         return HttpResponse(status=200)
     except Exception as e:
+        messages.error(request, f'Something Went Wrong With:  (e)')
         return HttpResponse(status=500)
