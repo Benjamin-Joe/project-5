ba9aa1aabe5e146e531870c92b329c162e0983c0
Benjamin-Joe <benjaminjoe43@gmail.com>
2022-02-26 16:46:16 +0000

    Add toat feature for messages

--- bag/views.py
+++ bag/views.py
@@ -1,14 +1,20 @@
 "Views.py In Bag App"
 from django.shortcuts import HttpResponse, render, reverse, redirect
+from django.contrib import messages
+from products.models import Product
 
 
 def add_to_bag(request, item_id):
     "Adding multiple items to shopping bag"
+
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
+        messages.success(request, f'Added {product.title} to your bag')
+
     else:
         bag[item_id] = quantity
     request.session['bag'] = bag
