535d668d3610f1bd5eeb8d838c840ad72f0efd64
Benjamin-Joe <benjaminjoe43@gmail.com>
2022-02-27 15:05:23 +0000

    Add stripe functionality for payments, following stripe documents and boutique ado stripe payments

--- checkout/views.py
+++ checkout/views.py
@@ -1,22 +1,42 @@
 "Views File In Checkout App"
 from django.shortcuts import render, reverse, redirect
 from django.contrib import messages
+from django.conf import settings
 from .forms import OrderForm
+from bag.contexts import bag_contents
+
+import stripe
 
 
 def checkout(request):
     "View for the checkout menu"
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, 'Nothing In Basket')
         return redirect(reverse('products'))
 
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
     order_form = OrderForm()
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51JhgJpKGApoDuOAh8jmkNakFGFYSc75EyTwrsQt2Nj1xLKgtkfbDXPvKFUJs848ovL1P8CoxP12beexihc0K2zka00Nb2w8P9Y',
-        'client_secret': 'testingclientsecret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
 
     }
     return render(request, template, context)
