70cb55aafdf7ef2950d8d42705b7d4da4ebfe70c
Benjamin-Joe <benjaminjoe43@gmail.com>
2022-02-28 15:47:30 +0000

    Add user update profile form and content to profile.html

--- profiles/models.py
+++ profiles/models.py
@@ -6,10 +6,13 @@ from django_countries.fields import CountryField
 
 
 class UserProfile(models.Model):
-    "Class for customer profiles"
+    """
+    A user profile model for maintaining default
+    delivery information and order history
+    """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     default_phone_number = models.CharField(max_length=20, null=True, blank=True)
-    default_country = CountryField(blank_label='Countries', null=True, blank=True)
+    default_country = CountryField(blank_label='Country', null=True, blank=True)
     default_postcode = models.CharField(max_length=20, null=True, blank=True)
     default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
     default_street_address1 = models.CharField(max_length=80, null=True, blank=True)
@@ -19,9 +22,13 @@ class UserProfile(models.Model):
     def __str__(self):
         return self.user.username
 
-    @receiver(post_save, sender=User)
-    def create_or_update_user_profile(sender, instance, created, **kwargs):
-        "Creating or changing profile info"
-        if created:
-            UserProfile.objects.create(userinstance)
-            instance.userprofile.save()
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    """
+    Create or update the user profile
+    """
+    if created:
+        UserProfile.objects.create(user=instance)
+    # Existing users: just save the profile
+    instance.userprofile.save()
